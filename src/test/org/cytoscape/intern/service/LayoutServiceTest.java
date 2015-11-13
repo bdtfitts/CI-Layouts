285dacaff67610203d439259810ccbb2d6ecb95f
Braxton Fitts <bdtfitts@outlook.com>
2015-11-12 17:56:25 -0800

    Added JUnit test skeleton

---
+++ src/test/org/cytoscape/intern/service/LayoutServiceTest.java
@@ -0,0 +1,14 @@
+package test.org.cytoscape.intern.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LayoutServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
